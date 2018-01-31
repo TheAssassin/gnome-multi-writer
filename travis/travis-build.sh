412be7c0acdbebe4e7ca6aa332809f4d8ccffc88
TheAssassin <theassassin@users.noreply.github.com>
2018-01-31 12:47:38 +0100

    Update build script

--- travis/travis-build.sh
+++ travis/travis-build.sh
@@ -4,7 +4,7 @@ set -e
 set -x
 
 apt-get update
-apt-get install -y python3-pip ninja-build libgtk-3-dev libgusb-dev libudisks2-dev libcanberra-dev libcanberra-dev libcanberra-gtk-dev
+apt-get install -y python3-pip ninja-build libgtk-3-dev libgusb-dev libudisks2-dev libcanberra-dev libcanberra-dev libcanberra-gtk3-dev
 pip3 install meson
 
 mkdir build/
